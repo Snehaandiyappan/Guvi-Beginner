2d25502ebac6b3d186ce4d716b7c88c1a258d09e
Snehaandiyappan <41100598+Snehaandiyappan@users.noreply.github.com>
2018-07-11 18:51:13 +0530

    Create postivenegative.java

---
+++ postivenegative.java
@@ -0,0 +1,19 @@
+class Ideone
+{
+	public static void main (String[] args) //throws java.lang.Exception
+	{
+		// your code goes 
+		int a=0;
+		if(a>=1){
+			System.out.println("positive");
+			
+		}
+		else if(a<0){
+			System.out.println("negavtive");
+		}
+		else{
+			System.out.println("ZERO");
+		}
+	}
+}
+		
